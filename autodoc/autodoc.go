f1e02a5f7684bec0bc61785ad675062b0b06f5eb
Lennart Weller <lhw@ring0.de>
2022-05-11 20:33:19 +0200

    update to provider sdk v2

--- autodoc/autodoc.go
+++ autodoc/autodoc.go
@@ -61,7 +61,7 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Exit status constants
--- validation/resource_validation.go
+++ validation/resource_validation.go
@@ -5,7 +5,7 @@ package validation
 import (
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // DiffSuppressStringIgnoreCase suppresses a Resource's schema diff for a
